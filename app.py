f77cc52455285910ee5ec654c1c6ffff16ebff84
Mahdewa <brahmamahdewa03@gmail.com>
2025-12-12 00:35:24 +0700

    update

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image, ImageOps
 import io
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -67,4 +68,5 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
